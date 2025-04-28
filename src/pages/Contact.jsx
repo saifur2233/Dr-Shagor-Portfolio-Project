10362799c0c0b725ebfbf13c58b38ff030e6e8c6
saifur2233 <saifurnstuiit223344@gmail.com>
2025-04-28 11:20:50 +0600

    Image added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -162,8 +162,8 @@ const Contact = () => {
                 <h2 className="hidden">Contact map</h2>
                 <div className="wpo-contact-map">
                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.9147703055!2d-74.11976314309273!3d40.69740344223377!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY%2C+USA!5e0!3m2!1sen!2sbd!4v1547528325671"
-                        allowFullScreen></iframe>
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3663.8228526252133!2d91.27771397515!3d23.32218527896485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x37537fadeda516fd%3A0xc47252218c125ef!2zTm93YWJhemFyIEJ1cyBTdGFuZCDgpqjgp4vgp5_gpr7gpqzgpr7gppzgpr7gprAg4Kas4Ka-4Ka4IOCmuOCnjeCmn-CnjeCmr-CmvuCmqOCnjeCmoQ!5e0!3m2!1sen!2sbd!4v1745817451555!5m2!1sen!2sbd"
+                        allowFullScreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </section>
 
